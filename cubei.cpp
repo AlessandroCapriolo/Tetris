f8e52fb439ad0a03744e38872fce31d2614cb3df
AlessandroCapriolo <capriolo.alessandro@googlemail.com>
2015-03-28 15:59:18 +0100

    Create cubei.cpp

---
+++ cubei.cpp
@@ -0,0 +1,103 @@
+#include "cubei.h"
+using namespace cubeiNS;
+
+CubeI::CubeI() : GenericCube()
+{
+
+}
+
+//=============================================================================
+// place chuncks in vecPieces in the right coordinate
+//=============================================================================
+void CubeI::setPieces()
+{
+	vecPieces[0]->setX(genericCubeNS::MAP_X + N1_CELL_X * blockNS::WIDTH);			//180
+	vecPieces[0]->setY(genericCubeNS::MAP_Y + N1_CELL_Y * blockNS::WIDTH);
+
+	vecPieces[1]->setX(genericCubeNS::MAP_X + N2_CELL_X * blockNS::WIDTH);
+	vecPieces[1]->setY(genericCubeNS::MAP_Y + N2_CELL_Y * blockNS::WIDTH);
+
+	vecPieces[2]->setX(genericCubeNS::MAP_X + N3_CELL_X * blockNS::WIDTH);
+	vecPieces[2]->setY(genericCubeNS::MAP_Y + N3_CELL_Y * blockNS::WIDTH);
+
+	vecPieces[3]->setX(genericCubeNS::MAP_X + N4_CELL_X * blockNS::WIDTH);
+	vecPieces[3]->setY(genericCubeNS::MAP_Y + N4_CELL_Y * blockNS::WIDTH);
+
+	for (auto b : vecPieces)
+		b->color = Block::CYAN;
+}
+
+//=============================================================================
+// rotate the cube checking the map
+//=============================================================================
+void CubeI::rotate(std::vector<std::vector<std::shared_ptr<Block>>>& map)
+{
+	int cnk_x = 0;
+	int cnk_y = 0;
+
+	switch (orient)
+	{
+	case GenericCube::NORTH:
+	case GenericCube::SOUTH:
+		cnk_x = static_cast<int>((vecPieces[2]->getX() - genericCubeNS::MAP_X) / blockNS::WIDTH);
+		cnk_y = static_cast<int>((vecPieces[2]->getY() - genericCubeNS::MAP_Y) / blockNS::HEIGHT);
+		if (cnk_y == 19 || cnk_y < 2)
+			return;
+		if (map[cnk_y - 2][cnk_x] != nullptr || map[cnk_y - 1][cnk_x] != nullptr || map[cnk_y + 1][cnk_x] != nullptr)
+			return;
+
+		vecPieces[0]->setX(vecPieces[0]->getX() + 2 * blockNS::WIDTH);			
+		vecPieces[0]->setY(vecPieces[0]->getY() - 2 * blockNS::WIDTH);
+
+		vecPieces[1]->setX(vecPieces[1]->getX() + 1 * blockNS::WIDTH);
+		vecPieces[1]->setY(vecPieces[1]->getY() - 1 * blockNS::WIDTH);
+
+		vecPieces[3]->setX(vecPieces[3]->getX() - 1 * blockNS::WIDTH);
+		vecPieces[3]->setY(vecPieces[3]->getY() + 1 * blockNS::WIDTH);
+
+		orient = EAST;
+
+		break;
+	case GenericCube::EAST:
+	case GenericCube::WEST:
+		cnk_x = static_cast<int>((vecPieces[2]->getX() - genericCubeNS::MAP_X) / blockNS::WIDTH);
+		cnk_y = static_cast<int>((vecPieces[2]->getY() - genericCubeNS::MAP_Y) / blockNS::HEIGHT);
+		if (cnk_x < 2 || cnk_x == 9)
+			return;
+		if (map[cnk_y][cnk_x - 2] != nullptr || map[cnk_y][cnk_x - 1] != nullptr || map[cnk_y][cnk_x + 1] != nullptr)
+			return;
+
+		vecPieces[0]->setX(vecPieces[0]->getX() - 2 * blockNS::WIDTH);
+		vecPieces[0]->setY(vecPieces[0]->getY() + 2 * blockNS::WIDTH);
+
+		vecPieces[1]->setX(vecPieces[1]->getX() - 1 * blockNS::WIDTH);
+		vecPieces[1]->setY(vecPieces[1]->getY() + 1 * blockNS::WIDTH);
+
+		vecPieces[3]->setX(vecPieces[3]->getX() + 1 * blockNS::WIDTH);
+		vecPieces[3]->setY(vecPieces[3]->getY() - 1 * blockNS::WIDTH);
+
+		orient = NORTH;
+
+		break;
+	default:
+		break;
+	}
+}
+
+//=============================================================================
+// update each chunck
+//=============================================================================
+void CubeI::update(float frameTime)
+{
+	for (auto b : vecPieces)
+		b->update(frameTime);
+}
+
+//=============================================================================
+// draw all chunks in vecPieces
+//=============================================================================
+void CubeI::draw()
+{
+	for (auto b : vecPieces)
+		b->draw();
+}
